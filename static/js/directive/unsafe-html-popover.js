b07c37bc5e171f6ffe88691a48010efa9cbda47e
laxika <laxika91@gmail.com>
2016-01-08 00:28:51 +0100

    Fixed inventory popup

--- static/js/directive/unsafe-html-popover.js
+++ static/js/directive/unsafe-html-popover.js
@@ -3,7 +3,11 @@ angular.module( 'ui.bootstrap.popover' )
         return {
             restrict: 'EA',
             replace: true,
-            scope: { title: '@', content: '@', placement: '@', animation: '&', isOpen: '&' },
+            scope: { title: '@', content: '@', placement: '@', animation: '&', isOpen: '&'},
+            controller: function($scope){
+                //Ugly hack to access to the parent scope this popup sits in.
+                $scope.data = $scope.$parent.$parent.$parent;
+            },
             template: '<div class="popover {{placement}}" ng-class="{ in: isOpen(), fade: animation() }"><div class="arrow"></div><div class="popover-inner"><h3 class="popover-title" bind-html-compile="title" ng-show="title"></h3><div class="popover-content" bind-html-compile="content"></div></div></div>'
         };
     })
