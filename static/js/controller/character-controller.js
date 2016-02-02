f020dab974d366629416f1a4edec29e67f9e542e
laxika <laxika91@gmail.com>
2016-02-02 22:09:20 +0100

    Added base for scavenging

--- static/js/controller/character-controller.js
+++ static/js/controller/character-controller.js
@@ -3,8 +3,6 @@
 module.exports = function ($scope, $http, $state, ATTRIBUTE_BONUS_MAP, characterData, $rootScope, characterDataFormatter) {
     $scope.user = characterData;
     $scope.attributeBonusNameMap = ATTRIBUTE_BONUS_MAP;
-    $scope.scavengingSlider = 0;
-    $scope.maxScavengingPointsToConvert = Math.floor((50 - $scope.user.scavengingPoints) / 5) * 5;
 
     $scope.inventoryPopover = require('html/popover/inventory-popover.html');
     $scope.attributePopover = require('html/popover/attribute-popover.html');
@@ -30,47 +28,11 @@ module.exports = function ($scope, $http, $state, ATTRIBUTE_BONUS_MAP, character
         return require('image/combat/icon/' + attribute + '.png');
     };
 
-    $scope.calculateMaxScavengingPointsToConvert = function () {
-        var pountsUntilMax = Math.floor((50 - $scope.user.scavengingPoints) / 5) * 5;
-
-        if (pountsUntilMax / 5 > $rootScope.user.movement) {
-            pountsUntilMax = $rootScope.user.movement * 5;
-        }
-
-        return pountsUntilMax;
-    };
-
-    $scope.convertScavengingPoints = function () {
-        if ($scope.scavengingSlider == 0) {
-            return;
-        }
-
-        var requestConfig = {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        };
-
-        var requestData = {
-            pointsToConvert: $scope.scavengingSlider / 5
-        };
-
-        $http.post('/skill/scavenging/convert', $.param(requestData), requestConfig).success(function (data, status, headers, config) {
-            $rootScope.$broadcast('profile-update-needed');
-        });
-    };
-
     $scope.$on('profile-update-needed', function (event, args) {
         $http.get('http://api.daggersandsorcery.com/character/info').then(function (response) {
             $scope.user = characterDataFormatter.format(response.data);
         });
-
-        $scope.scavengingSlider = 0;
     });
-
-    $scope.calculateNeededMovementPoints = function (scavengingSlider) {
-        return Math.ceil(scavengingSlider / 5);
-    };
 
     $scope.unequip = function (slot) {
         $http.get('http://api.daggersandsorcery.com/unequip/' + slot).then(function (response) {
