e96607cce7ea7a48d0a4de2012f012db4b9e1f49
laxika <laxika91@gmail.com>
2016-02-07 23:04:29 +0100

    Added attribute descriptions for combat attributes.

--- static/js/controller/character-controller.js
+++ static/js/controller/character-controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function ($scope, $http, $state, ATTRIBUTE_BONUS_MAP, characterData, $rootScope, characterDataFormatter) {
+module.exports = function ($scope, $http, $state, $sce, ATTRIBUTE_BONUS_MAP, characterData, $rootScope, characterDataFormatter) {
     $scope.user = characterData;
     $scope.attributeBonusNameMap = ATTRIBUTE_BONUS_MAP;
 
@@ -10,6 +10,35 @@ module.exports = function ($scope, $http, $state, ATTRIBUTE_BONUS_MAP, character
     $scope.incrementedAttributePopover = require('html/popover/incremented-attribute-popover.html');
     $scope.equipmentPopover = require('html/popover/equipment-popover.html');
     $scope.spellPopover = require('html/popover/spell-popover.html');
+
+    $scope.combatAttributeDescriptionMap = function(attribute) {
+        switch(attribute) {
+            case 'MANA':
+                return $sce.trustAsHtml("Mana points are used for spellcasting. A starting character's MP is 0 + half of the <b><i>intelligence</i></b>, <b><i>wisdom</i></b>, <b><i>charisma</i></b>, <b><i>willpower</i></b> attributes.");
+            case 'LIFE':
+                return $sce.trustAsHtml("Life points shows the amount of damage a character can receive before dying. If the character's LP reach 0 then it dies. The base life point is 15 + half of the <b><i>vitality</i></b> and <b><i>endurance</i></b> attributes.");
+            case 'INITIATION':
+                return $sce.trustAsHtml("The initiation attribute determines who attack first in combat. The starting initiation is 5 + half of the <b><i>swiftness</i></b> and <b><i>perception</i></b> attributes.");
+            case 'ATTACK':
+                return $sce.trustAsHtml("The attack attribute determines the chance of damaging the enemy in meele combat. The starting attack is 5 + half of the <b><i>dexterity</i></b> and <b><i>swiftness</i></b>.");
+            case 'MAGIC_ATTACK':
+                return $sce.trustAsHtml("The magic attack attribute determines the chance of damaging the enemy in magic combat. The starting attack is 5 + half of the <b><i>intelligence</i></b> and <b><i>wisdom</i></b>.");
+            case 'AIMING':
+                return $sce.trustAsHtml("The aiming attribute determines the chance of damaging the enemy in ranged combat. The starting aiming is 5 + half of the <b><i>dexterity</i></b> and <b><i>perception</i></b>.");
+            case 'DAMAGE':
+                return $sce.trustAsHtml("The damage attribute determines the amount of casualty the character can deal to it's opponent in meele combat. The starting damage is 1 + the quarter of the <b><i>strength</i></b> attribute.");
+            case 'RANGED_DAMAGE':
+                return $sce.trustAsHtml("The damage attribute determines the amount of casualty the character can deal to it's opponent in ranged combat. The starting ranged damage is 1 + the quarter of the <b><i>dexterity</i></b> attribute.");
+            case 'MAGIC_DAMAGE':
+                return $sce.trustAsHtml("The damage attribute determines the amount of casualty the character can deal to it's opponent in magical combat. The starting ranged damage is 1 + the quarter of the <b><i>willpower</i></b> attribute.");
+            case 'DEFENSE':
+                return $sce.trustAsHtml("The defense attribute determines the chance of avoiding the opponents attacks in combat. The starting defense is 5 + half of the <b><i>endurance</i></b> and <b><i>dexterity</i></b>.");
+            case 'SPELL_RESISTANCE':
+                return $sce.trustAsHtml("The spell resistance attribute determines the chance of avoiding or decreasing the opponents magical attacks or damage in combat. The starting spell resistance is 5 + half of the <b><i>willpower</i></b> and <b><i>intelligence</i></b>.");
+            default:
+                return $sce.trustAsHtml("No description <b><i>(yet)</i></b>.");
+        }
+    };
 
     $scope.type = 'empty';
     $scope.setType = function(newType) {
