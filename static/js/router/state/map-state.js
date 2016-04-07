369ec1d420b80c163547a38d716f9b3468bcbc19
Gyula Lakatos <v-glakatos@hotels.com>
2016-04-07 15:14:55 +0200

    Working on explore

--- static/js/router/state/map-state.js
+++ static/js/router/state/map-state.js
@@ -4,37 +4,11 @@ module.exports = {
     name: 'map',
     url: '/map/',
     template: require('partial/main/map.html'),
-    /*resolve: {
-        position: function ($http) {
-            return $http.get('http://api.daggersandsorcery.com/character/position').then(function(response) {
-                return response.data.data;
+    controller: function ($scope, $http, $state) {
+        $scope.explore = function(explorationId) {
+            $http.get('http://api.daggersandsorcery.com/explore/').success(function (data, status, headers, config) {
+                console.log(data);
             });
         }
-    },*/
-    controller: function ($scope, $http, $state) {
-        /*$scope.position = position;
-        $scope.objects = position.spawnList;
-
-        $scope.move = function(direction) {
-            var directionData = {
-                'direction': direction
-            };
-
-            $http.post('http://api.daggersandsorcery.com/character/move', directionData).success(function (data, status, headers, config) {
-                if(data.data.success) {
-                    $scope.$broadcast('position', data.data);
-                }
-
-                $scope.objects = data.data.spawnList;
-            });
-        };
-
-        $scope.attack = function(target) {
-            $http.get('http://api.daggersandsorcery.com/map/combat/'+target).success(function (data, status, headers, config) {
-                if(!data.error) {
-                    $state.go('combat', {'combatMessages': data.combatMessages});
-                }
-            });
-        }*/
     }
 };
