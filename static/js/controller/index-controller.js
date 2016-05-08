8906ac80b8734c6d098c9f5622dc36dcabcfe687
Laxika <laxika91@gmail.com>
2016-05-08 23:28:48 +0200

    DS-200: News using liquibase.

--- static/js/controller/index-controller.js
+++ static/js/controller/index-controller.js
@@ -2,7 +2,7 @@
 
 module.exports = function ($scope, $http, $sce, newslist) {
      for (var i = 0; i < newslist.data.length; i++) {
-         newslist.data[i].message = $sce.trustAsHtml(newslist.data[i].message);
+         //newslist.data[i].message = $sce.trustAsHtml(newslist.data[i].message);
          newslist.data[i].title = $sce.trustAsHtml(newslist.data[i].title);
      }
 
