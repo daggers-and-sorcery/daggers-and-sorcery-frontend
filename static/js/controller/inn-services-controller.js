53f92989318f08c09ff06f441ba11e8b8462e0ff
Gyula Lakatos <laxika91@gmail.com>
2016-05-16 00:10:24 +0200

    DS-227: Inn services comes from server side.

--- static/js/controller/inn-services-controller.js
+++ static/js/controller/inn-services-controller.js
@@ -3,7 +3,13 @@
 module.exports = function ($scope, $state, innInfo) {
     console.log(innInfo);
 
-    $scope.showChat = function() {
+    $scope.inn = innInfo;
+
+    $scope.getServiceImage = function (service) {
+        return require('image/inn/' + service + '.jpg');
+    };
+
+    $scope.showChat = function () {
         $state.go('inn');
     }
 };
