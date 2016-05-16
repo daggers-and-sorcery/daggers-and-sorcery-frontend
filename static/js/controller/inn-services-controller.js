980bad993345e6d4210dd3baa656cd7fe4436e1c
Gyula Lakatos <laxika91@gmail.com>
2016-05-16 19:43:40 +0200

    The player can order servings from the inn

--- static/js/controller/inn-services-controller.js
+++ static/js/controller/inn-services-controller.js
@@ -1,15 +1,19 @@
 'use strict';
 
-module.exports = function ($scope, $state, innInfo) {
-    console.log(innInfo);
-
+module.exports = function ($scope, $state, $http, innInfo) {
     $scope.inn = innInfo;
 
     $scope.getServiceImage = function (service) {
-        return require('image/inn/' + service.toLowerCase().replace("_", "-") + '.jpg');
+        return require('image/inn/' + service.toLowerCase().replace(/_/g, "-") + '.jpg');
     };
 
     $scope.showChat = function () {
         $state.go('inn');
+    };
+
+    $scope.orderService = function (serviceId) {
+        $http.get('http://api.daggersandsorcery.com/inn/service/'+serviceId).then(function(response) {
+            console.log("Handleresponse")
+        });
     }
 };
