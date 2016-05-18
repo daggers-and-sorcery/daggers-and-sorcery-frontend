969135e6f96cdafa7acf697ed05b2cb390360bef
Laxika <laxika91@gmail.com>
2016-05-18 22:03:42 +0200

    DS-227: Finishing touches on the inn.

--- static/js/controller/inn-controller.js
+++ static/js/controller/inn-controller.js
@@ -1,13 +1,32 @@
 'use strict';
 
-module.exports = function ($scope, $state, chatList) {
-    $scope.messages = chatList;
+module.exports = function ($scope, $state, $interval, $http, chatList) {
+    $scope.messages = chatList.messages;
+    $scope.textToSend = "";
 
-    $scope.writeMessage = function() {
-       //TODO: send
+    var refreshChat = function() {
+        $http.get('http://api.daggersandsorcery.com/inn/chat/list').then(function (response) {
+            $scope.messages = response.data.data.messages;
+        });
     };
 
-    $scope.showServices = function() {
+    $interval(function () {
+        refreshChat();
+    }, 5000);
+
+    $scope.writeMessage = function () {
+        var payload = {
+            chatMessage: $scope.textToSend
+        };
+
+        $http.post('http://api.daggersandsorcery.com/inn/chat/send', payload).then(function (response) {
+            refreshChat();
+        });
+
+        $scope.textToSend = "";
+    };
+
+    $scope.showServices = function () {
         $state.go('inn-services');
     }
 };
