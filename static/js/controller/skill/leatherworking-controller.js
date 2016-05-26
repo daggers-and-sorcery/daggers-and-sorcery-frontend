b15cc57c303f5b581ca3ef5c2207c4b2895af912
Laxika <laxika91@gmail.com>
2016-05-26 22:13:58 +0200

    DS-180: More work on leatherworking

--- static/js/controller/skill/leatherworking-controller.js
+++ static/js/controller/skill/leatherworking-controller.js
@@ -1,4 +1,17 @@
 'use strict';
 
-module.exports = function ($scope) {
+module.exports = function ($scope, $http, $rootScope, curingRecipeList) {
+    $scope.leatherworkingInfo = curingRecipeList;
+
+    //TODO: Add the code for switching to leathercrafting
+
+    $scope.create = function (recipeId) {
+        var payload = {
+            recipeId: recipeId
+        };
+
+        $http.post('http://api.daggersandsorcery.com/skill/cooking/create', payload).success(function (data, status, headers, config) {
+        }).error(function (data, status, headers, config) {
+        });
+    }
 };
--- static/js/router/state/skill/leatherworking-state.js
+++ static/js/router/state/skill/leatherworking-state.js
@@ -4,10 +4,12 @@ module.exports = {
     name: 'skill-leatherworking',
     url: '/skill/leatherworking/',
     template: require('partial/main/skill/leatherworking.html'),
-    /*resolve: {
-        cookingInfo: function ($http) {
-            return $http({method: 'GET', url: 'http://api.daggersandsorcery.com/skill/leatherworking/info'});
+    resolve: {
+        curingRecipeList: function ($http) {
+            return $http({method: 'GET', url: 'http://api.daggersandsorcery.com/skill/leatherworking/curing/info'}).then(function(response) {
+                return response.data.data;
+            });
         }
-    },*/
+    },
     controller: require('js/controller/skill/leatherworking-controller.js')
 };
