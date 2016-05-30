a36dafe776c57413c80ac7ccd55c1c3e775b28e4
Laxika <laxika91@gmail.com>
2016-05-30 23:55:45 +0200

    DS-280: More work on curing.

--- static/js/controller/skill/leatherworking-controller.js
+++ static/js/controller/skill/leatherworking-controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function ($scope, $http, $rootScope, curingRecipeList) {
+module.exports = function ($scope, $http, $rootScope, $interval, curingRecipeList) {
     $scope.leatherworkingInfo = curingRecipeList;
 
     //TODO: Add the code for switching to leathercrafting
@@ -11,7 +11,32 @@ module.exports = function ($scope, $http, $rootScope, curingRecipeList) {
         };
 
         $http.post('http://api.daggersandsorcery.com/skill/leatherworking/curing/start', payload).success(function (data, status, headers, config) {
-        }).error(function (data, status, headers, config) {
+            $scope.refresh();
         });
-    }
+    };
+
+    $scope.refresh = function () {
+        $http({
+            method: 'GET',
+            url: 'http://api.daggersandsorcery.com/skill/leatherworking/curing/info'
+        }).then(function (response) {
+            $scope.leatherworkingInfo = response.data.data;
+        });
+    };
+
+    $scope.timer = $interval(function () {
+        angular.forEach($scope.leatherworkingInfo.curing_list, function (value, key) {
+            $scope.leatherworkingInfo.curing_list[key].timeLeft -= 1000;
+
+            if($scope.leatherworkingInfo.curing_list[key].timeLeft < 1000) {
+                $scope.refresh();
+            }
+        });
+    }, 1000);
+
+    $scope.$on("$destroy", function () {
+        if (angular.isDefined($scope.timer)) {
+            $interval.cancel($scope.timer);
+        }
+    });
 };
