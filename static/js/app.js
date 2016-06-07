aecfae06fa42c702e816e06cc94ad5944b2e5710
Laxika <laxika91@gmail.com>
2016-06-07 22:23:00 +0200

    DS-280: Finished tanning.

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = angular.module('swordssorcery', ['ui.router', 'ui.bootstrap', 'ngMessages', 'rzModule', 'hc.marked', 'ngFlash', 'hm.readmore'])
+module.exports = angular.module('swordssorcery', ['ui.router', 'ui.bootstrap', 'ngMessages', 'rzModule', 'hc.marked', 'ngAnimate', 'ngFlash', 'hm.readmore'])
     .config(function ($httpProvider) {
         $httpProvider.defaults.withCredentials = true;
     })
