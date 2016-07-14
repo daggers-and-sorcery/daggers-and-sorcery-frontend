529b7ae989de15e0bc97f02183f62cf9f073f50a
Laxika <laxika91@gmail.com>
2016-07-14 22:41:39 +0200

    DS-216: Reworked the exploration screen to be easily extendable.

--- static/js/router/state/map-state.js
+++ static/js/router/state/map-state.js
@@ -12,12 +12,23 @@ module.exports = {
         }
     },
     controller: function ($scope, $state, explorationInfo) {
-        if(explorationInfo.events.length > 0) {
+        if (explorationInfo.events.length > 0) {
             $state.go('explore', {'explorationInfo': explorationInfo});
         }
 
-        $scope.explore = function(explorationId) {
+        $scope.getExplorationImage = function (imageId) {
+            return require('image/exploration/' + imageId + '.png');
+        };
+
+        $scope.explore = function (explorationId) {
             $state.go('explore', {'explorationId': explorationId, 'nextStage': 0});
-        }
+        };
+
+        $scope.switchDescription = function(index) {
+            $scope.explorationData.location.exploration[index].showfull = !$scope.explorationData.location.exploration[index].showfull;
+        };
+
+        $scope.explorationData = require('data/explore/sevgard.xml');
+        console.log($scope.explorationData)
     }
 };
