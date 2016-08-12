c57d829396f38911490cfa45eb2af17d8686dc11
Gyula Lakatos <v-glakatos@hotels.com>
2016-08-12 12:45:55 +0200

    Continous combat added

--- static/js/controller/explore-controller.js
+++ static/js/controller/explore-controller.js
@@ -12,6 +12,14 @@ module.exports = function ($scope, $http, $sce, $state, $stateParams, exploratio
     $scope.attack = function() {
         $http.get('http://api.daggersandsorcery.com/combat/attack').then(function (response) {
             $scope.explorationResult.events[$scope.explorationResult.events.length - 1].combatMessages = $scope.explorationResult.events[$scope.explorationResult.events.length - 1].combatMessages.concat(response.data.data.combatSteps);
+            $scope.explorationResult.events[$scope.explorationResult.events.length - 1].status = response.data.data.status;
+        });
+    };
+
+    $scope.continueExploring = function() {
+        $http.get('http://api.daggersandsorcery.com/explore/next').then(function (response) {
+            $scope.explorationResult.events[$scope.explorationResult.events.length - 1].status.continued = true;
+            $scope.explorationResult.events = $scope.explorationResult.events.concat(response.data.data.events);
         });
     };
 
