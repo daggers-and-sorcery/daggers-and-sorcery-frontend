7f7ba5223915c7a2fa3557c81243ae84d87727ae
laxika <laxika91@gmail.com>
2016-08-17 22:48:30 +0200

    Added support for flee

--- static/js/controller/explore-controller.js
+++ static/js/controller/explore-controller.js
@@ -5,16 +5,16 @@ module.exports = function ($scope, $http, $sce, $state, $stateParams, exploratio
     $scope.usableSpells = [];
     $scope.usableItems = [];
     $scope.spellToUse = {};
-    $scope.itemToUse = {}
+    $scope.itemToUse = {};
 
-    $http.get('http://api.daggersandsorcery.com/combat/settings/usable/spell').then(function (response) {
+    $http.get('http://api.daggersandsorcery.com/combat/usable/spell').then(function (response) {
         $scope.usableSpells = response.data.data.spellList;
         if ($scope.usableSpells.length > 0) {
             $scope.spellToUse.target = $scope.usableSpells[0];
         }
     });
 
-    $http.get('http://api.daggersandsorcery.com/combat/settings/usable/item').then(function (response) {
+    $http.get('http://api.daggersandsorcery.com/combat/usable/item').then(function (response) {
         $scope.usableItems = response.data.data.itemList;
         if ($scope.usableItems.length > 0) {
             $scope.itemToUse.target = $scope.usableItems[0];
@@ -23,6 +23,20 @@ module.exports = function ($scope, $http, $sce, $state, $stateParams, exploratio
 
     $scope.cast = function () {
         $http.get('http://api.daggersandsorcery.com/combat/cast/' + $scope.spellToUse.target.id).then(function (response) {
+            $scope.explorationResult.events[$scope.explorationResult.events.length - 1].combatMessages = $scope.explorationResult.events[$scope.explorationResult.events.length - 1].combatMessages.concat(response.data.data.combatSteps);
+            $scope.explorationResult.events[$scope.explorationResult.events.length - 1].status = response.data.data.status;
+        });
+    };
+
+    $scope.use = function () {
+        $http.get('http://api.daggersandsorcery.com/combat/use/' + $scope.itemToUse.target.id).then(function (response) {
+            $scope.explorationResult.events[$scope.explorationResult.events.length - 1].combatMessages = $scope.explorationResult.events[$scope.explorationResult.events.length - 1].combatMessages.concat(response.data.data.combatSteps);
+            $scope.explorationResult.events[$scope.explorationResult.events.length - 1].status = response.data.data.status;
+        });
+    };
+
+    $scope.flee = function () {
+        $http.get('http://api.daggersandsorcery.com/combat/flee').then(function (response) {
             $scope.explorationResult.events[$scope.explorationResult.events.length - 1].combatMessages = $scope.explorationResult.events[$scope.explorationResult.events.length - 1].combatMessages.concat(response.data.data.combatSteps);
             $scope.explorationResult.events[$scope.explorationResult.events.length - 1].status = response.data.data.status;
         });
