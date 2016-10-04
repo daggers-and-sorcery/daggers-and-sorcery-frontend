17c9e100e24f798cd04e0dc0e389e1e428c2817b
laxika <laxika91@gmail.com>
2016-10-04 08:44:40 +0200

    Topiclist finally working

--- static/js/controller/forum/forum-comment-list-controller.js
+++ static/js/controller/forum/forum-comment-list-controller.js
@@ -2,5 +2,20 @@
 
 module.exports = function ($scope, $http, comments) {
     $scope.comments = comments;
-    console.log(comments);
+
+    $scope.newMessageContent = '';
+
+    $scope.newComment = function() {
+        console.log("new comm");
+
+        var payload = {
+            content: $scope.newMessageContent,
+            parentTopic: $scope.comments.parent.id
+        };
+
+
+        console.log(payload);
+
+        $http.post('http://api.daggersandsorcery.com/forum/new_comment', payload);
+    };
 };
