e410e2ed25e981fd76abc02c1e982e9eea4ece30
laxika <laxika91@gmail.com>
2016-10-05 20:55:54 +0200

    The forum is done

--- static/js/controller/forum/forum-comment-list-controller.js
+++ static/js/controller/forum/forum-comment-list-controller.js
@@ -6,16 +6,17 @@ module.exports = function ($scope, $http, comments) {
     $scope.newMessageContent = '';
 
     $scope.newComment = function() {
-        console.log("new comm");
-
         var payload = {
             content: $scope.newMessageContent,
             parentTopic: $scope.comments.parent.id
         };
 
-
-        console.log(payload);
-
-        $http.post('http://api.daggersandsorcery.com/forum/new_comment', payload);
+        $scope.newMessageContent = '';
+        $http.post('http://api.daggersandsorcery.com/forum/new_comment', payload).then(function (response) {
+                $http.get('http://api.daggersandsorcery.com/forum/list/topic/' + $scope.comments.parent.id).then(function (response) {
+                    $scope.comments = response.data.data;
+                })
+            }
+        );
     };
 };
--- static/js/controller/forum/forum-topic-list-controller.js
+++ static/js/controller/forum/forum-topic-list-controller.js
@@ -6,13 +6,18 @@ module.exports = function ($scope, $http, topics) {
     $scope.newTopicTitle = '';
     $scope.newTopicContent = '';
 
-    $scope.newTopic = function() {
+    $scope.newTopic = function () {
         var payload = {
             name: $scope.newTopicTitle,
             content: $scope.newTopicContent,
-            parentCategory: topics.parentCategory.id
+            parentCategory: $scope.topics.parentCategory.id
         };
 
-        $http.post('http://api.daggersandsorcery.com/forum/new_topic', payload);
+        $http.post('http://api.daggersandsorcery.com/forum/new_topic', payload).then(function (response) {
+                $http.get('http://api.daggersandsorcery.com/forum/list/category/' + $scope.topics.parentCategory.id).then(function (response) {
+                    $scope.topics = response.data.data;
+                })
+            }
+        );
     };
 };
