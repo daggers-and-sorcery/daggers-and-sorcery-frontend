0af8f8b0d497d877ea35491e110da799962091ac
laxika <laxika91@gmail.com>
2016-10-25 21:32:20 +0200

    Working on selling

--- static/js/controller/market/market-entry-sell-controller.js
+++ static/js/controller/market/market-entry-sell-controller.js
@@ -1,9 +1,31 @@
 'use strict';
 
-module.exports = function ($scope, marketData) {
+module.exports = function ($scope, $http, $log, marketData) {
     $scope.marketData = marketData;
 
-    $scope.sell = function () {
-        console.log("sell")
+    $scope.item = {
+        id: marketData.item.id,
+        amount: 1,
+        price: {
+            bronzePrice: 0,
+            silverPrice: 0,
+            goldPrice: 0
+        }
+    };
+
+    $scope.sellItem = function () {
+        $log.debug("Posting sell offer for item: " + $scope.item.id + " on market with amount: " + $scope.item.amount + " and price: " + angular.toJson($scope.item.price));
+
+        var payload = {
+            item: $scope.item.id,
+            amount: $scope.item.amount,
+            priceBronze: $scope.item.price.bronzePrice,
+            priceSilver: $scope.item.price.silverPrice,
+            priceGold: $scope.item.price.goldPrice
+        };
+
+        $http.post('http://api.daggersandsorcery.com/market/sell', payload).then(function (response) {
+            $log.debug("Got response for selling attempt!");
+        });
     };
 };
