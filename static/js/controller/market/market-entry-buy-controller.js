315db0fa68e2555928ef0a82cde0851d49b6f142
Gyula Lakatos <laxika91@gmail.com>
2016-11-07 19:26:41 +0100

    market improvements

--- static/js/controller/market/market-entry-buy-controller.js
+++ static/js/controller/market/market-entry-buy-controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function ($scope, $http, $log, marketData) {
+module.exports = function ($scope, $http, $log, Flash, marketData) {
     $scope.marketData = marketData;
 
     $scope.floor = function (value) {
@@ -15,7 +15,12 @@ module.exports = function ($scope, $http, $log, marketData) {
         };
 
         $http.post('http://api.daggersandsorcery.com/market/buy', payload).then(function (response) {
+            var buyingResult = response.data.data.result.result;
+
             $log.debug("Got response for buying attempt!");
+
+            Flash.clear();
+            Flash.create($scope.getFlashColorFromResponse(buyingResult), $scope.getFlashMessageFromResponse(buyingResult));
 
             $scope.refreshMarketData();
         });
@@ -25,9 +30,31 @@ module.exports = function ($scope, $http, $log, marketData) {
         $log.debug("Refreshing the buylist for item: " + $scope.marketData.item.id);
 
         $http.get('http://api.daggersandsorcery.com/market/show/buy/' + $scope.marketData.item.id).then(function (response) {
-            $log.debug("Buylist refresh finished.")
+            $log.debug("Buylist refresh finished.");
 
             $scope.marketData = response.data.data;
         });
+    };
+
+    $scope.getFlashMessageFromResponse = function (result) {
+        if (result === 'SUCCESSFUL_TRANSACTION') {
+            return "You successfully bought an item.";
+        } else if (result === 'INSUFFICIENT_RESOURCES') {
+            return "You don't have enough coins to buy this item.";
+        }
+
+        return "Unknown result!";
+    };
+
+    $scope.getFlashColorFromResponse = function (result) {
+        if ($scope.successfulResult(result)) {
+            return 'success';
+        }
+
+        return 'danger';
+    };
+
+    $scope.successfulResult = function (result) {
+        return result === 'SUCCESSFUL_TRANSACTION';
     }
 };
--- static/js/controller/market/market-entry-sell-controller.js
+++ static/js/controller/market/market-entry-sell-controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function ($scope, $http, $log, marketData) {
+module.exports = function ($scope, $http, $log, Flash, marketData) {
     $scope.marketData = marketData;
 
     $scope.item = {
@@ -32,20 +32,51 @@ module.exports = function ($scope, $http, $log, marketData) {
         if ($scope.sellForm.$valid) {
             $log.debug("The selling form is valid, doing the selling logic.");
 
-            if ($scope.display.maximumAmount - $scope.item.amount < 1) {
-                $scope.display.sellingFormDisabled = true;
-                $scope.display.maximumAmount = 0;
-
-                $log.debug("This was the last item to sell, disabling the selling form.");
-            } else {
-                $scope.display.maximumAmount -= $scope.item.amount;
-
-                $log.debug("Decreasing the sellable amount by one. The amount left to sell is: " + $scope.marketData.sellData.maximumAmount);
-            }
-
             $http.post('http://api.daggersandsorcery.com/market/sell', payload).then(function (response) {
-                $log.debug("Got response for selling attempt!");
+                var sellingResult = response.data.data.result.sellingResult;
+
+                $log.debug("Got response for selling attempt! The result is: " + sellingResult + ".");
+
+                if ($scope.successfulResult(sellingResult)) {
+                    if ($scope.display.maximumAmount - $scope.item.amount < 1) {
+                        $scope.display.sellingFormDisabled = true;
+                        $scope.display.maximumAmount = 0;
+
+                        $log.debug("This was the last item to sell, disabling the selling form.");
+                    } else {
+                        $scope.display.maximumAmount -= $scope.item.amount;
+
+                        $log.debug("Decreasing the sellable amount by one. The amount left to sell is: " + $scope.marketData.sellData.maximumAmount);
+                    }
+                }
+
+                Flash.clear();
+                Flash.create($scope.getFlashColorFromResponse(sellingResult), $scope.getFlashMessageFromResponse(sellingResult));
             });
         }
     };
+
+    $scope.getFlashMessageFromResponse = function (result) {
+        if (result === 'BAD_PRICE') {
+            return "Invalid price! The price must be higher than zero.";
+        } else if (result === 'SUCCESSFUL_TRANSACTION') {
+            return "You successfully posted an item for sale.";
+        } else if (result === 'INSUFFICIENT_RESOURCES') {
+            return "You don't have any from the item!";
+        }
+
+        return "Unknown result!";
+    };
+
+    $scope.getFlashColorFromResponse = function (result) {
+        if ($scope.successfulResult(result)) {
+            return 'success';
+        }
+
+        return 'danger';
+    };
+
+    $scope.successfulResult = function (result) {
+        return result === 'SUCCESSFUL_TRANSACTION';
+    }
 };
