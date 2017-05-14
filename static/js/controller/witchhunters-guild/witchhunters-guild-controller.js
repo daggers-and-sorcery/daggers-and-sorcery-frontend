fb5d971b11599f1c7ad753998144ab05759d5fdf
laxika <laxika91@gmail.com>
2017-05-15 00:27:49 +0200

    More witchhunters guild improvments

--- static/js/controller/witchhunters-guild/witchhunters-guild-controller.js
+++ static/js/controller/witchhunters-guild/witchhunters-guild-controller.js
@@ -1,4 +1,9 @@
 'use strict';
 
 module.exports = function ($scope) {
+    $scope.subtitle = 'Guild hall';
+
+    $scope.setSubtitle = function (subtitle) {
+        $scope.subtitle = subtitle;
+    }
 };
