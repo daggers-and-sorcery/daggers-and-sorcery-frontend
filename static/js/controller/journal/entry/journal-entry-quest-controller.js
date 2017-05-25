b0744bb6c2cdeebb9fe6679834ec6ca0837eed96
Gyula Lakatos <v-glakatos@hotels.com>
2017-05-25 19:49:50 +0200

    Working on the journal entries for quests

--- static/js/controller/journal/entry/journal-entry-quest-controller.js
+++ static/js/controller/journal/entry/journal-entry-quest-controller.js
@@ -1,4 +1,5 @@
 'use strict';
 
-module.exports = function ($scope) {
+module.exports = function ($scope, journalEntryInfo) {
+    $scope.entry = journalEntryInfo;
 };
--- static/js/router/state/journal/entry/journal-entry-quest-state.js
+++ static/js/router/state/journal/entry/journal-entry-quest-state.js
@@ -3,6 +3,14 @@
 module.exports = {
     name: 'journal-entry.journal-entry-quest',
     url: 'quest/:entryId',
+    resolve: {
+        journalEntryInfo: function ($http, $stateParams) {
+            return $http.get('https://api.daggersandsorcery.com/journal/entry/quest/' + $stateParams.entryId)
+                .then(function (response) {
+                    return response.data.data.journalEntryInfo;
+                });
+        }
+    },
     template: require('partial/main/journal/entry/journal-entry-quest.html'),
     controller: require('js/controller/journal/entry/journal-entry-quest-controller.js')
 };
