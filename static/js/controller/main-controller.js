c5268ef28e91186938878d7db227ac9a9aca244e
laxika <laxika91@gmail.com>
2017-06-28 22:09:15 +0200

    Added loader

--- static/js/controller/main-controller.js
+++ static/js/controller/main-controller.js
@@ -83,6 +83,12 @@ module.exports = require('js/app.js').controller('MainController', function ($sc
             event.preventDefault();
             $state.go('home');
         }
+
+        $scope.stateIsLoading = true;
+    });
+
+    $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+        $scope.stateIsLoading = false;
     });
 
     $scope.user = {};
