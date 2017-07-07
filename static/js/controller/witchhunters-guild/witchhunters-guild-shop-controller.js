aa1056c4705f2f637a8c0767d337a7eca8f4b2b8
Gyula Lakatos <v-glakatos@hotels.com>
2017-07-07 08:58:29 +0200

    Finished the withchunters shop

--- static/js/controller/witchhunters-guild/witchhunters-guild-shop-controller.js
+++ static/js/controller/witchhunters-guild/witchhunters-guild-shop-controller.js
@@ -1,4 +1,19 @@
 'use strict';
 
-module.exports = function ($scope) {
+module.exports = function ($scope, $http) {
+
+    $scope.buyItem = function (itemId) {
+        $http.get('https://api.daggersandsorcery.com/shop/' + $scope.shopData.definition.id + '/buy/' + itemId).then(function () {
+            $scope.refreshBuyShop();
+        });
+    };
+
+    $scope.refreshBuyShop = function () {
+        //TODO: Shopid will come from guild rank
+        $http.get('https://api.daggersandsorcery.com/shop/buylist/2').then(function (response) {
+            $scope.shopData = response.data.data;
+        })
+    };
+
+    $scope.refreshBuyShop();
 };
