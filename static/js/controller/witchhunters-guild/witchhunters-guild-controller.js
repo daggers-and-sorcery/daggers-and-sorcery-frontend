7c77aa78143bf19a99f0879bc1ee23510b39ba8e
Gyula Lakatos <v-glakatos@hotels.com>
2017-07-18 09:07:20 +0200

    Curing the thirsty screen pops up on wh guild when player is a vampire

--- static/js/controller/witchhunters-guild/witchhunters-guild-controller.js
+++ static/js/controller/witchhunters-guild/witchhunters-guild-controller.js
@@ -1,7 +1,14 @@
 'use strict';
 
-module.exports = function ($scope) {
-    $scope.subtitle = 'Guild hall';
+module.exports = function ($scope, $state, witchhuntersGuildInfo) {
+    $scope.showGuild = witchhuntersGuildInfo.info.accessible;
+
+    if ($scope.showGuild) {
+        $scope.subtitle = 'Guild hall';
+        $state.go('witchhunters-guild.witchhunters-guild-main');
+    } else {
+        $scope.subtitle = 'Entrance';
+    }
 
     $scope.setSubtitle = function (subtitle) {
         $scope.subtitle = subtitle;
--- static/js/router/state/witchhunters-guild/witchhunters-guild-state.js
+++ static/js/router/state/witchhunters-guild/witchhunters-guild-state.js
@@ -3,6 +3,14 @@
 module.exports = {
     name: 'witchhunters-guild',
     url: '/witchhunters-guild/',
+    resolve: {
+        witchhuntersGuildInfo: function ($http) {
+            return $http.get('https://api.daggersandsorcery.com/witchhunters-guild/info')
+                .then(function (response) {
+                    return response.data.data;
+                });
+        }
+    },
     template: require('partial/main/witchhunters-guild/witchhunters-guild.html'),
     controller: require('js/controller/witchhunters-guild/witchhunters-guild-controller.js')
 };
