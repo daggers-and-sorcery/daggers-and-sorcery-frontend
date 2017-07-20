ec0a42cd4ebfb7b7d6a1765a4cbbb6f24a0ae507
laxika <laxika91@gmail.com>
2017-07-20 19:24:56 +0200

    Finished the witchhunters guild

--- static/js/controller/witchhunters-guild/witchhunters-guild-controller.js
+++ static/js/controller/witchhunters-guild/witchhunters-guild-controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function ($scope, $state, witchhuntersGuildInfo) {
+module.exports = function ($scope, $http, $state, witchhuntersGuildInfo) {
     $scope.showGuild = witchhuntersGuildInfo.info.accessible;
     $scope.showQuest = !witchhuntersGuildInfo.info.questStarted;
 
@@ -11,11 +11,21 @@ module.exports = function ($scope, $state, witchhuntersGuildInfo) {
         $scope.subtitle = 'Entrance';
     }
 
+    $scope.acceptVampireQuest = function () {
+        var payload = {
+            questId: 2
+        };
+
+        $http.post('https://api.daggersandsorcery.com/quest/accept', payload).then(function (response) {
+            $scope.showQuest = false;
+        });
+    };
+
     $scope.setSubtitle = function (subtitle) {
         $scope.subtitle = subtitle;
-    }
+    };
 
-    $scope.hideQuest = function() {
+    $scope.hideQuest = function () {
         $scope.showQuest = false;
     }
 };
