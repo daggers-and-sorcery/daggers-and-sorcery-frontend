effbf1d3852de4692e2f039bc506e402ef699284
laxika <laxika91@gmail.com>
2017-07-24 21:02:07 +0200

    Fixed a query bug on the wh guild.

--- static/js/controller/witchhunters-guild/witchhunters-guild-shop-controller.js
+++ static/js/controller/witchhunters-guild/witchhunters-guild-shop-controller.js
@@ -3,20 +3,24 @@
 module.exports = function ($scope, $http, shopInfo) {
     $scope.shopInfo = shopInfo;
 
-    $scope.isShopAvailable = function() {
+    $scope.isShopAvailable = function () {
         return $scope.shopInfo.shop.shopId !== -1;
-    }
+    };
 
     $scope.buyItem = function (itemId) {
-        $http.get('https://api.daggersandsorcery.com/shop/' + $scope.shopInfo.shop.shopId + '/buy/' + itemId).then(function () {
-            $scope.refreshBuyShop();
-        });
+        if ($scope.isShopAvailable()) {
+            $http.get('https://api.daggersandsorcery.com/shop/' + $scope.shopInfo.shop.shopId + '/buy/' + itemId).then(function () {
+                $scope.refreshBuyShop();
+            });
+        }
     };
 
     $scope.refreshBuyShop = function () {
-        $http.get('https://api.daggersandsorcery.com/shop/buylist/'+$scope.shopInfo.shop.shopId).then(function (response) {
-            $scope.shopData = response.data.data;
-        })
+        if ($scope.isShopAvailable()) {
+            $http.get('https://api.daggersandsorcery.com/shop/buylist/' + $scope.shopInfo.shop.shopId).then(function (response) {
+                $scope.shopData = response.data.data;
+            })
+        }
     };
 
     $scope.refreshBuyShop();
