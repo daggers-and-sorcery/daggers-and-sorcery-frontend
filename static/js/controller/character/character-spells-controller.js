2ca24a431564a676929bcc7f2d7660d899d405c6
laxika <laxika91@gmail.com>
2017-09-11 14:39:37 +0200

    Spell types are visible on the spells description

--- static/js/controller/character/character-spells-controller.js
+++ static/js/controller/character/character-spells-controller.js
@@ -3,5 +3,10 @@
 module.exports = function ($scope, spells) {
     $scope.spellPopover = require('html/popover/definition/spell-definition-popover.html');
 
+    $scope.getSpellTypeIcon = function(spellType) {
+        console.log("getting img for" +spellType);
+        return require('image/spell/type/icon/' + spellType.toLowerCase() + '.png');
+    };
+
     $scope.spells = spells;
 };
