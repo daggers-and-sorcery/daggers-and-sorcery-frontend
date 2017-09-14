70a1391a922f5d1764c37f487e3d5f2ee425b45d
laxika <laxika91@gmail.com>
2017-09-14 11:49:05 +0200

    Added images to more of the items

--- static/js/directive/definition/item-definition-display-directive.js
+++ static/js/directive/definition/item-definition-display-directive.js
@@ -15,23 +15,11 @@ require('js/app.js').directive('itemDefinitionDisplay', function () {
             scope.itemDefinitionPopover = require('html/popover/definition/item-definition-popover.html');
 
             scope.hasImage = function () {
-                return scope.item.equipment
-                    || scope.item.id === 1
-                    || scope.item.id === 2
-                    || scope.item.id === 3
-                    || scope.item.id === 4
-                    || scope.item.id === 5
-                    || scope.item.id === 7
-                    || scope.item.id === 8
-                    || scope.item.id === 11
-                    || scope.item.id === 12
-                        //TODO: From this!
-                    || scope.item.id === 25
-                    || scope.item.id === 49
-                    || scope.item.id === 50
-                    || scope.item.id === 58
-                    || scope.item.id === 74
-                    || scope.item.id === 111;
+                try {
+                    return __webpack_modules__[require.resolveWeak('image/inventory/item/' + scope.item.id + '.png')];
+                } catch (err) {
+                    return false;
+                }
             };
 
             scope.getItemImage = function () {
