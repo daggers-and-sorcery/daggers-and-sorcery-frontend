3b0ae0ebcdaa326480fe10dd1843bcbc0e33df63
laxika <laxika91@gmail.com>
2017-09-19 00:18:59 +0200

    Fixed displaying of unidentified items

--- static/js/directive/definition/item-definition-display-directive.js
+++ static/js/directive/definition/item-definition-display-directive.js
@@ -8,13 +8,18 @@ require('js/app.js').directive('itemDefinitionDisplay', function () {
         },
         template: require('html/directive/definition/item-definition-display-directive.html'),
         link: function (scope, element, attrs) {
-            if(scope.display === undefined) {
+            if (scope.display === undefined) {
                 scope.display = 'text';
             }
 
             scope.itemDefinitionPopover = require('html/popover/definition/item-definition-popover.html');
 
             scope.hasImage = function () {
+                console.log(scope.item.id);
+                if (scope.item.id > 1000) {
+                    return true;
+                }
+
                 try {
                     return __webpack_modules__[require.resolveWeak('image/inventory/item/' + scope.item.id + '.png')];
                 } catch (err) {
@@ -23,6 +28,10 @@ require('js/app.js').directive('itemDefinitionDisplay', function () {
             };
 
             scope.getItemImage = function () {
+                if (scope.item.id > 1000) {
+                    return require('image/icon/item.png');
+                }
+
                 return require('image/inventory/item/' + scope.item.id + '.png');
             };
         }
